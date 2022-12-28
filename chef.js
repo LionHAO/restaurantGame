974d6be2f20c563cb4c7d831022b8d5417d96e5f
LionHAO <cch3291387738@163.com>
2022-12-28 09:59:28 +0800

    object分组件

---
+++ chef.js
@@ -0,0 +1,15 @@
+class Chef{
+  // constructor({
+  //   imgUrl,
+  // }){
+  //   this.imgUrl=imgUrl;
+  // }
+  // #init = () => {
+  //   const chef = document.createElement('div');
+  //   chef.classList.add('chef');
+  //   const img = document.createElement('img');
+  //   img.src = this.imageUrl;
+  //   chef.append(img);
+  //   this.element = chef;
+  // }
+}

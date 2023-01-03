a6b352c71049d02d2fe645ebc59394fe2e827a60
LionHAO <cch3291387738@163.com>
2023-01-03 11:20:16 +0800

    顾客落座完成

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const chefWorking = []//储存正在工作的厨师的序号
 const customerWaitList = []
 const customerWaitNodeList = []
 
+const customerSeatList = []
+const customerSeatNodeList = []
+
 const chefBoxPlace = document.querySelector(".small-inline-Box")
 const button = document.querySelectorAll("button")
 const chefPlace = document.querySelector(".chefPlace")
@@ -20,11 +23,14 @@ const confirmRecruitment = document.querySelector(".confirmRecruitment");//确�
 const noRecruitment = document.querySelector(".noRecruitment");//先不招聘
 const confirmFry = document.querySelector(".confirmFry");//确认解雇
 const noFry = document.querySelector(".noFry");//先不解雇
+const customerSeats = document.querySelectorAll(".grid-item")
+
+
 let fryNum;//炒掉第几个厨师
 let chefNode;//厨师节点，为了删除厨师
 //测试
 button[0].addEventListener("click", (e) => {
-    buyChef()
+  buyChef()
 })
 
 button[1].addEventListener("click", (e) => {
@@ -45,7 +51,42 @@ button[3].addEventListener("click", (e) => {
   continueTime()
 })
 
+{/*     <div class="grid-item">
+          <div class="seat">
+            <img class="seatImg" src="/asset/customer2.png" />
+          </div>
+          <div class="orderdishes">
+            <p class="orderdish">UI炖UI</p>
+            <p class="orderdish">UI炖UI</p>
+            <p class="orderdish">UI炖UI</p>
+        </div> */}
+button[4].addEventListener("click", () => {
+  for (const i in customerSeats) {
+    const element = customerSeats[i];
+    //如果位置没有图片,则是空位
+    if (!element.children[0].children[0]) {
+      new CustomerSeat(istc = "/asset/customer2.png", node = element)
+      break
+    }
+  }
+})
 
+class CustomerSeat {
+  constructor(isrc, node) {
+    //两个参数 一个是图片路径,一个是node节点(why do this?)[4个节点都是一样的])
+    this.isrc = isrc
+    this.node = node
+    this.#init()
+  }
+
+  #init() {
+    var seatImg = document.createElement('img')
+    seatImg.classList.add('seatImg')
+    seatImg.src = this.isrc
+    console.log(node);
+    node.children[0].appendChild(seatImg)
+  }
+}
 
 class Customer {
   constructor() {
@@ -99,151 +140,150 @@ class Customer {
     var waitingBar = customerWaitNodeList[index].children[0]
     //给宽进行递增
     if (!waitingBar.children[0].style.width) {
-      console.log('here');
       waitingBar.children[0].style.width = '0%'
     }
     var width = parseInt(waitingBar.children[0].style.width.match(/\d+/)[0]) + 1
     waitingBar.children[0].style.width = width + '%'
     if (width >= 100) {
-      customerWaitNodeList.splice(index,1)
-      customerWaitList.splice(index,1)
+      customerWaitNodeList.splice(index, 1)
+      customerWaitList.splice(index, 1)
     }
   }
 }
 
 
 class Chef {
-    constructor(isBusy,  workable) {
-        this.isBusy = isBusy //忙吗
-        this.workable = workable //能不能干活
-        this.#init();
-    }
-
-    //私有属性
-    #init = () => {//每new一个Chef 就会做这件事,动态创建好一个这玩意
-        // 定义一个厨师的dom元素其实一个厨师,然后往里面加属性,像是盘子,X等,动态创建
-        const chef = document.createElement('div');
-
-        chef.classList.add("chefBox")//等同于 chef.setAttribute('class',chefBox); 都是将创造的div赋予class="chefBox"
-
-        //给他脸,给脸附上class(为了样式)
-        const img = document.createElement('img');
-        img.src = "asset/chef.png";
-        img.classList.add("addCookerImg")
-        // img.style='filter:drop-shadow( 0px 0 0px white);' 可以显示阴影但是盖不住
-
-        //别删注释
-        //   <div class='chefBox' id='chefBox1'>
-        //   <img class='addCookerImg' src='asset/chef.png' alt='cooker'>
-        //   <!-- 做菜进度条 -->
-        //   <div class='cookingBar'>
-        //     <div class='shade'>
-        //       <p class='dish'>UI炖UI</p>
-        //     </div>
-        //   </div>
-        //   <!-- 递盘子 -->
-        //   <img class='plateImg' src='asset/food.png' alt='plate'>
-        //   <!-- 删除厨师 -->
-        //   <p class="del-chef">×</p>
-        //   <!-- 添加厨师 -->
-        //   <p class="save-chef">＋</p>
-        // </div>
-
-        //都是创建元素,添加class属性名字,然后动态添加
-
-        //进度条
-        const cookingBar = document.createElement("div");
-        cookingBar.classList.add("cookingBar");
-        const shade = document.createElement("div")
-        shade.classList.add("shade")
-        const dish = document.createElement("p")
-        dish.append("UI炖UI")
-        dish.classList.add("dish")
-        cookingBar.appendChild(shade)
-        shade.appendChild(dish)
-        cookingBar.style.display = "none"
-
-
-        //托盘
-        const plateImg = document.createElement("img");
-        plateImg.src = 'asset/food.png'
-        plateImg.classList.add("plateImg");
-        plateImg.style.display = "none"
-
-        //删除x
-        const delChef = document.createElement("p");
-        delChef.append("×")
-        delChef.classList.add("del-chef");
-        delChef.setAttribute("data-index",chefNodeList.length)
-        delChef.style.display = "none"
-        delChef.addEventListener("click", (e) => {
-            fryChef1.style.display="block";
-            blackShadow.style.display="block";
-            chefNode=delChef.parentNode;
-            fryNum = e.target.dataset.index;
-            console.log(fryNum+"---------");
-            console.log(e.target);
-        })
-
-
-        // 添加
-        const saveChef = document.createElement("div");
-        saveChef.append("＋")
-        saveChef.classList.add("save-chef");
-        saveChef.addEventListener("click", (e) => {
-            buyChef1.style.display="block";
-            blackShadow.style.display="block";
-        })
-
-
-        // 这个顺序很重要,之后每次进行新增都会加入到chefNodeList中,可以在chefNodeList中使用[]来根据下标取值
-        // 然后如果还要细细的进行某个子节点的操作(比如进度条,比如托盘显示),就要用到如chefNodeList[4].children[0](表示取第4位厨师的头像);
-        // 其中的children是他的所有子元素,顺序如下
-        // [img.addCookerImg, div.cookingBar, img.plateImg, p.del-chef, div.save-chef]
-
-        chef.appendChild(img);
-        chef.appendChild(cookingBar)
-        chef.appendChild(plateImg)
-        chef.appendChild(delChef)
-        chef.appendChild(saveChef)
-
-
-        //放入厨师的那块区域
-        //目前采用的这种方法还没考虑到后面的属性变化(比如进度条如果要改变的话该怎么处理),不知是否可行
-        chefBoxPlace.appendChild(chef);
-
-        chefNodeList.push(chef)
-    }
-
- //这个函数的工作逻辑是每一次调用进度条都会涨一点,需要外部调用实现增长不会自增
- cooking = () => {
-  //获取这个对象在抽象数组中的位置
-  var index = chefList.indexOf(this)
-  //获取进度条对象
-  var cookingBar = chefNodeList[index].children[1]
-
-  //判断是不是第一次进来,
-  if (!this.isBusy) {
-    //设置这个厨师忙着呢
-    this.isBusy = true
-    //解禁display
-    cookingBar.style.display = 'block'
-    //这里后面要根据菜品修改,把咸鱼改成传入的参数
-    cookingBar.children[0].children[0].innerText = "咸鱼"
-    cookingBar.children[0].style.width = '0%'
-    cookingBar.children[0].style.background = 'red'
+  constructor(isBusy, workable) {
+    this.isBusy = isBusy //忙吗
+    this.workable = workable //能不能干活
+    this.#init();
   }
-  //给宽进行递增
-  var width = parseInt(cookingBar.children[0].style.width.match(/\d+/)[0]) + 1
-  cookingBar.children[0].style.width = width + '%'
-  if (width >= 100) {
-    chefNodeList[index].children[2].style.display = 'block';
-    this.isBusy = false//忙完了,不忙了
-    this.workable = false//但是手里端着菜盘子,不能干活
-    chefWorking.splice(index, 1)
+
+  //私有属性
+  #init = () => {//每new一个Chef 就会做这件事,动态创建好一个这玩意
+    // 定义一个厨师的dom元素其实一个厨师,然后往里面加属性,像是盘子,X等,动态创建
+    const chef = document.createElement('div');
+
+    chef.classList.add("chefBox")//等同于 chef.setAttribute('class',chefBox); 都是将创造的div赋予class="chefBox"
+
+    //给他脸,给脸附上class(为了样式)
+    const img = document.createElement('img');
+    img.src = "asset/chef.png";
+    img.classList.add("addCookerImg")
+    // img.style='filter:drop-shadow( 0px 0 0px white);' 可以显示阴影但是盖不住
+
+    //别删注释
+    //   <div class='chefBox' id='chefBox1'>
+    //   <img class='addCookerImg' src='asset/chef.png' alt='cooker'>
+    //   <!-- 做菜进度条 -->
+    //   <div class='cookingBar'>
+    //     <div class='shade'>
+    //       <p class='dish'>UI炖UI</p>
+    //     </div>
+    //   </div>
+    //   <!-- 递盘子 -->
+    //   <img class='plateImg' src='asset/food.png' alt='plate'>
+    //   <!-- 删除厨师 -->
+    //   <p class="del-chef">×</p>
+    //   <!-- 添加厨师 -->
+    //   <p class="save-chef">＋</p>
+    // </div>
+
+    //都是创建元素,添加class属性名字,然后动态添加
+
+    //进度条
+    const cookingBar = document.createElement("div");
+    cookingBar.classList.add("cookingBar");
+    const shade = document.createElement("div")
+    shade.classList.add("shade")
+    const dish = document.createElement("p")
+    dish.append("UI炖UI")
+    dish.classList.add("dish")
+    cookingBar.appendChild(shade)
+    shade.appendChild(dish)
+    cookingBar.style.display = "none"
+
+
+    //托盘
+    const plateImg = document.createElement("img");
+    plateImg.src = 'asset/food.png'
+    plateImg.classList.add("plateImg");
+    plateImg.style.display = "none"
+
+    //删除x
+    const delChef = document.createElement("p");
+    delChef.append("×")
+    delChef.classList.add("del-chef");
+    delChef.setAttribute("data-index", chefNodeList.length)
+    delChef.style.display = "none"
+    delChef.addEventListener("click", (e) => {
+      fryChef1.style.display = "block";
+      blackShadow.style.display = "block";
+      chefNode = delChef.parentNode;
+      fryNum = e.target.dataset.index;
+      console.log(fryNum + "---------");
+      console.log(e.target);
+    })
+
+
+    // 添加
+    const saveChef = document.createElement("div");
+    saveChef.append("＋")
+    saveChef.classList.add("save-chef");
+    saveChef.addEventListener("click", (e) => {
+      buyChef1.style.display = "block";
+      blackShadow.style.display = "block";
+    })
+
+
+    // 这个顺序很重要,之后每次进行新增都会加入到chefNodeList中,可以在chefNodeList中使用[]来根据下标取值
+    // 然后如果还要细细的进行某个子节点的操作(比如进度条,比如托盘显示),就要用到如chefNodeList[4].children[0](表示取第4位厨师的头像);
+    // 其中的children是他的所有子元素,顺序如下
+    // [img.addCookerImg, div.cookingBar, img.plateImg, p.del-chef, div.save-chef]
+
+    chef.appendChild(img);
+    chef.appendChild(cookingBar)
+    chef.appendChild(plateImg)
+    chef.appendChild(delChef)
+    chef.appendChild(saveChef)
+
+
+    //放入厨师的那块区域
+    //目前采用的这种方法还没考虑到后面的属性变化(比如进度条如果要改变的话该怎么处理),不知是否可行
+    chefBoxPlace.appendChild(chef);
+
+    chefNodeList.push(chef)
   }
-  //每0.1s加1,到92结束
-}
+
+  //这个函数的工作逻辑是每一次调用进度条都会涨一点,需要外部调用实现增长不会自增
+  cooking = () => {
+    //获取这个对象在抽象数组中的位置
+    var index = chefList.indexOf(this)
+    //获取进度条对象
+    var cookingBar = chefNodeList[index].children[1]
+
+    //判断是不是第一次进来,
+    if (!this.isBusy) {
+      //设置这个厨师忙着呢
+      this.isBusy = true
+      //解禁display
+      cookingBar.style.display = 'block'
+      //这里后面要根据菜品修改,把咸鱼改成传入的参数
+      cookingBar.children[0].children[0].innerText = "咸鱼"
+      cookingBar.children[0].style.width = '0%'
+      cookingBar.children[0].style.background = 'red'
+    }
+    //给宽进行递增
+    var width = parseInt(cookingBar.children[0].style.width.match(/\d+/)[0]) + 1
+    cookingBar.children[0].style.width = width + '%'
+    if (width >= 100) {
+      chefNodeList[index].children[2].style.display = 'block';
+      this.isBusy = false//忙完了,不忙了
+      this.workable = false//但是手里端着菜盘子,不能干活
+      chefWorking.splice(index, 1)
+    }
+    //每0.1s加1,到92结束
+  }
 }
 
 
@@ -268,7 +308,7 @@ buyChef = () => {
     //开局加一个
     chefList.push(new Chef(isBusy = false, workable = false))//也可以写成Chef(false,true)只是比较好
 
-    } else {
+  } else {
     //先把占位厨师的+去掉,给他加上x(删除符号),然后创建一个占位厨师
     chefNodeList[chefNodeList.length - 1].children[4].style.display = "none"
     chefNodeList[chefNodeList.length - 1].children[3].style.display = "initial"
@@ -284,63 +324,63 @@ buyChef = () => {
 
 //解雇厨师
 function fryChef() {
-    if (chefNodeList.length == 2) {
-        //如果只剩下一位厨师就不能解雇了
-        chefNodeList[1].children[4].style.display = "none"
-        chefNodeList[1].children[3].style.display = "none"
-        chefNodeList[2].children[4].style.display = "initial"
-        chefNodeList[2].children[3].style.display = "none"
-    }  else {
-        chefNodeList.re
-    }
-    //大于三位厨师框框变大
-    if (chefNodeList.length > 3) {
-        chefPlace.style.height = '38%'
-    }
-    return true;
+  if (chefNodeList.length == 2) {
+    //如果只剩下一位厨师就不能解雇了
+    chefNodeList[1].children[4].style.display = "none"
+    chefNodeList[1].children[3].style.display = "none"
+    chefNodeList[2].children[4].style.display = "initial"
+    chefNodeList[2].children[3].style.display = "none"
+  } else {
+    chefNodeList.re
+  }
+  //大于三位厨师框框变大
+  if (chefNodeList.length > 3) {
+    chefPlace.style.height = '38%'
+  }
+  return true;
 }
 //确认招聘
 confirmRecruitment.addEventListener("click", (e) => {
-    console.log(e);
-    buyChef1.style.display="none";
-    blackShadow.style.display="none";
-    buyChef()
+  console.log(e);
+  buyChef1.style.display = "none";
+  blackShadow.style.display = "none";
+  buyChef()
 })
 //先不招聘
 noRecruitment.addEventListener("click", (e) => {
-    buyChef1.style.display="none";
-    blackShadow.style.display="none";
+  buyChef1.style.display = "none";
+  blackShadow.style.display = "none";
 })
 //先不解雇
 noFry.addEventListener("click", (e) => {
-    fryChef1.style.display="none";
-    blackShadow.style.display="none";
+  fryChef1.style.display = "none";
+  blackShadow.style.display = "none";
 })
 confirmFry.addEventListener("click", (e) => {
   console.log(chefNodeList.length);
   fryChef()
-  fryChef1.style.display="none";
-  blackShadow.style.display="none";
+  fryChef1.style.display = "none";
+  blackShadow.style.display = "none";
   console.log(chefNodeList.length);
 })
 
 //解雇厨师
 function fryChef() {
-    if (chefNodeList.length == 2) {
-        //如果只剩下一位厨师就不能解雇了
-        chefNodeList[1].children[4].style.display = "none"
-        chefNodeList[1].children[3].style.display = "none"
-        chefNodeList[2].children[4].style.display = "initial"
-        chefNodeList[2].children[3].style.display = "none"
-    }  else {
-      chefNodeList.splice(fryNum,1);
-      chefBoxPlace.removeChild(chefNode);
-    }
-    //大于三位厨师框框变大
-    if (chefNodeList.length > 3) {
-        chefPlace.style.height = '38%'
-    }
-    return true;
+  if (chefNodeList.length == 2) {
+    //如果只剩下一位厨师就不能解雇了
+    chefNodeList[1].children[4].style.display = "none"
+    chefNodeList[1].children[3].style.display = "none"
+    chefNodeList[2].children[4].style.display = "initial"
+    chefNodeList[2].children[3].style.display = "none"
+  } else {
+    chefNodeList.splice(fryNum, 1);
+    chefBoxPlace.removeChild(chefNode);
+  }
+  //大于三位厨师框框变大
+  if (chefNodeList.length > 3) {
+    chefPlace.style.height = '38%'
+  }
+  return true;
 }
 
 //监视厨师工作,其实就是监视 chefWorking 这个数组,
